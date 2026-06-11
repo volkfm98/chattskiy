feb8f28da955df94a67f3bff55b2298f2f6c1291
Pavel Volkov <volkfm98@gmail.com>
2026-06-11 11:39:32 +0500

    Feat: Add snowflake id generator

--- src/main/java/ru/volkfm/chattskiy/config/ApplicationConfig.java
+++ src/main/java/ru/volkfm/chattskiy/config/ApplicationConfig.java
@@ -1,8 +1,13 @@
 package ru.volkfm.chattskiy.config;
 
+import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import ru.volkfm.chattskiy.generator.SnowflakeIdGenerator;
 
 @Configuration
 public class ApplicationConfig {
-
+    @Bean
+    public SnowflakeIdGenerator snowflakeIdGenerator() {
+        return new SnowflakeIdGenerator(1L);
+    }
 }
